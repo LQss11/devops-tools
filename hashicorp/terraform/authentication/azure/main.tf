c444761ac295b2abb41969057e511c82641a37ba
Salem Affes <affessalem@hotmail.fr>
2022-12-14 09:29:25 +0100

    Azure with terraform credentials

---
+++ hashicorp/terraform/authentication/azure/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+# Either uncomment and create tfvars file to define variables
+# Or pass environment variables (read the readme file)
+provider "azurerm" {
+  features {}
+#   subscription_id = var.subscription_id
+#   client_id       = var.client_id
+#   tenant_id       = var.tenant_id
+#   client_secret   = var.client_secret
+}
---
+++ hashicorp/terraform/authentication/azure/variables.tf
@@ -0,0 +1,19 @@
+variable "client_id" {
+  type        = string
+  description = "Service Principle App ID"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Service Principle Tenant ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Service Principle Password"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Service Principle Subscription ID"
+}
