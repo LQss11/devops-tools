5411867ca3dd553974fc43a9955f705c3e7c6be6
Affes Salem <49565791+LQss11@users.noreply.github.com>
2023-01-24 01:35:46 +0100

    Update for dialog

--- docker/images/data/openai/chatgpt/chatgpt.sh
+++ docker/images/data/openai/chatgpt/chatgpt.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # API_KEY="YOURTOKEN" ./docker/images/data/openai/chatgpt/chatgpt.sh "hello"
-# curl -s https://raw.githubusercontent.com/LQss11/devops-tools/main/docker/images/data/openai/chatgpt/chatgpt.sh | API_KEY="YOURTOKEN" bash -s -- can you help me bot?
+# curl -s https://raw.githubusercontent.com/LQss11/devops-tools/main/docker/images/data/openai/chatgpt/chatgpt.sh | API_KEY="YOURTOKEN" bash
+
+#!/bin/bash
 
 # Set the OpenAI API key
 API_KEY="$API_KEY"
 
 # Set the prompt for the completion
-prompt="$*"
 # Set the model engine to use
 model_engine="text-davinci-003"
 
@@ -16,20 +17,27 @@ temperature=0.5
 max_tokens=4000
 presence_penalty=0.0
 
-# Create the JSON data for the API request
-data=$(jq -n \
-    --arg prompt "$prompt" \
-    --argjson temperature "$temperature" \
-    --argjson max_tokens "$max_tokens" \
-    --argjson presence_penalty "$presence_penalty" \
-    '{prompt: $prompt, temperature: $temperature, max_tokens: $max_tokens, presence_penalty: $presence_penalty}')
+while true; do
+    # get the user input
+    read -p "User:" userInput
+    # Create the JSON data for the API request
+    data=$(jq -n \
+        --arg prompt "$userInput" \
+        --argjson temperature "$temperature" \
+        --argjson max_tokens "$max_tokens" \
+        --argjson presence_penalty "$presence_penalty" \
+        '{prompt: $prompt, temperature: $temperature, max_tokens: $max_tokens, presence_penalty: $presence_penalty}')
 
-# Send the API request and store the response
-response=$(curl -s -X POST \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $API_KEY" \
-    -d "$data" \
-    "https://api.openai.com/v1/engines/$model_engine/completions")
+    # Send the API request and store the response
+    response=$(curl -s -X POST \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $API_KEY" \
+        -d "$data" \
+        "https://api.openai.com/v1/engines/$model_engine/completions")
 
-# Extract the choice from the response and print it
-echo  "$response" | jq -r '.choices[0].text'
+    # Extract the choice from the response and print it
+    echo "########################"
+    echo "$response" | jq -r '.choices[0].text'
+    echo "########################"
+done
+
