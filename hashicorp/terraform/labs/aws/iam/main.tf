a46fa071a4fa919d028d739920ab233d1ddbb621
Salem Affes <affessalem@hotmail.fr>
2023-02-14 15:20:28 +0100

    Update acces

--- hashicorp/terraform/labs/aws/iam/main.tf
+++ hashicorp/terraform/labs/aws/iam/main.tf
@@ -2,11 +2,35 @@ resource "aws_iam_user" "users" {
   count = length(var.users)
   name  = var.users[count.index]
 }
+resource "aws_iam_policy" "restricted_iam_access" {
+  name = "restricted_iam_access"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:*"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Deny"
+        Action = [
+          "iam:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
 
 resource "aws_iam_user_policy_attachment" "user_admin_access" {
   count      = length(var.users)
   user       = aws_iam_user.users[count.index].name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  policy_arn = aws_iam_policy.restricted_iam_access.arn
+  # policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "aws_iam_access_key" "user_access_keys" {
