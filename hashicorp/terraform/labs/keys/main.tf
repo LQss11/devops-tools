170980f5628eb45d6ccb9ee7422aec734a09fadf
Salem Affes <affessalem@hotmail.fr>
2023-02-15 16:58:49 +0100

    Add key

---
+++ hashicorp/terraform/labs/keys/main.tf
@@ -0,0 +1,7 @@
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "mykey.pem"
+}
