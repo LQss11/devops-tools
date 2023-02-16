89b6ec0b29f09950f04533908e4fec30f37a21a5
Salem Affes <affessalem@hotmail.fr>
2023-02-16 15:41:01 +0100

    Web server instance variables

---
+++ hashicorp/terraform/labs/aws/multi-web-server/modules/instance/variables.tf
@@ -0,0 +1,5 @@
+variable "ssh_key_name" { type = string }
+variable "ami" { type = string }
+variable "instance_type" { type = string }
+variable "instance_name" { type = string }
+variable "private_key_pem" { type = string }
