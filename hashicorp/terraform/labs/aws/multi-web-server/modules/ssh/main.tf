4f9419fcb8dca8830bf9b32678e202e66f6350ef
Salem Affes <affessalem@hotmail.fr>
2023-02-16 15:41:14 +0100

    Web server instance ssh definition

---
+++ hashicorp/terraform/labs/aws/multi-web-server/modules/ssh/main.tf
@@ -0,0 +1,14 @@
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "${var.ssh_key_name}.pem"
+}
+resource "aws_key_pair" "ssh" {
+  key_name   = var.ssh_key_name
+  public_key = tls_private_key.generated.public_key_openssh
+  lifecycle {
+    ignore_changes = [key_name]
+  }
+}
