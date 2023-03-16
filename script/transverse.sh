eb5a3ffb85b7294e501ec5d3196488c7a0808e5c
Salem Affes <affessalem@hotmail.fr>
2023-03-16 11:11:23 +0100

    transverse file recursively

---
+++ script/transverse.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Define the array of file extensions to exclude
+exclude_ext=( "png" "jpg" "mp3" "mp4" "md" )
+
+# Get the first argument as the directory path
+dir_path="$1"
+
+# Check if the directory exists
+if [ ! -d "$dir_path" ]; then
+  echo "Directory not found!"
+  exit 1
+fi
+
+# Create an output file to write to
+output_file="output.txt"
+echo -n > "$output_file"
+
+# Traverse the directory and subdirectories recursively
+traverse_dir() {
+  for file in "$1"/*; do
+    if [ -d "$file" ]; then
+      traverse_dir "$file"
+    else
+      # Check if the file extension should be excluded
+      ext="${file##*.}"
+      if [[ " ${exclude_ext[@]} " =~ " $ext " ]]; then
+        continue
+      fi
+      echo "$file" >> "$output_file"
+      cat "$file" >> "$output_file"
+      echo -e "\n" >> "$output_file"
+    fi
+  done
+}
+
+# Call the traverse_dir function with the input directory path
+traverse_dir "$dir_path"
