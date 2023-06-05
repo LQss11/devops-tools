7beb821e5beafda47152532ad91daa3c1a9d2b25
Salem Affes <affessalem@hotmail.fr>
2023-06-05 16:47:45 +0100

    Remove unused files

--- linux/script/add-env-var-with-dot.sh
+++
@@ -1,2 +0,0 @@
-# Add env that contains characters that are not allowed  
-exec env spring.application_name=example-variable-name /bin/bash
--- linux/script/tree.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-##
-# This script print a directory tree of a project
-##
-find . -print | sed -e 's;[^/]*/;|--;g;s;--|;  |;g'
