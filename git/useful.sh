3816994351c1f5437d3f101aa4938e09349efd9f
Salem Affes <affessalem@hotmail.fr>
2023-06-07 16:10:30 +0100

    git commands for files perms

--- git/useful.sh
+++ git/useful.sh
@@ -1,2 +1,8 @@
 # Git is case insensitive for filenames to solve you can do this
 git mv File.txt file.txt
+
+# Update permissions of a file!: 
+git update-index --chmod=+x /path/to/file
+
+# Update permissions of a file and add if it was not commited first!: 
+git update-index --add --chmod=+x /path/to/file
