ad8add8ed64287cb2c3f039c18078c6c77240cb4
Salem Affes <affessalem@hotmail.fr>
2023-06-26 09:17:59 +0100

    Add init and provisioner commands

--- hashicorp/terraform/labs/aws/vm-ssh/main.tf
+++ hashicorp/terraform/labs/aws/vm-ssh/main.tf
@@ -1,8 +1,8 @@
 module "instance" {
+  instance_count = 3
   source         = "./modules/instance"
   ami            = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04 LTS
   instance_type  = "t2.micro"
-  instance_count = 3
   ssh_key_name   = var.ssh_key_name
   nsgs           = module.nsg.security_group_ids
 }
--- hashicorp/terraform/labs/aws/vm-ssh/modules/instance/main.tf
+++ hashicorp/terraform/labs/aws/vm-ssh/modules/instance/main.tf
@@ -5,10 +5,37 @@ resource "aws_instance" "main" {
 
   key_name = var.ssh_key_name
 
-  vpc_security_group_ids = var.nsgs
+  vpc_security_group_ids = concat(var.nsgs, [aws_security_group.allow_all.id])
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update"
+    ]
 
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${var.ssh_key_name}.pem")
+      host        = self.public_ip
+    }
+  }
+  # Will be executed once we launche the instance
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "ubuntu:ubuntu124" | chpasswd
+  EOF
   tags = {
     Name = "myawsinstance${count.index + 1}"
   }
 }
 
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow-all-outbound-nsg"
+  description = "Allow outbound traffic from all sources"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
