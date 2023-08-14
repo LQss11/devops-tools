6c721e613ddcffc61ab9fef59cb4da6d235f31a0
Salem Affes <affessalem@hotmail.fr>
2023-08-14 09:42:30 +0100

    Include error and fix listing header

--- hashicorp/vault/list_vault_secrets_to_markdown.sh
+++ hashicorp/vault/list_vault_secrets_to_markdown.sh
@@ -8,6 +8,9 @@ vault_data=$(vault kv list -format=json secret/$group)
 # Extract keys from the vault data
 keys=($(echo "$vault_data" | jq -r '.[]'))
 
+# Initialize the fields array
+declare -A fields
+
 # Initialize the associative array for modified data
 declare -A modified_data_array
 
@@ -19,64 +22,62 @@ for key in "${keys[@]}"; do
     if [ $? -eq 0 ]; then
         data=$(echo "$data" | jq -r '.data.data')
         modified_data=$(echo "$data" | jq --arg tool "$key" '. + { "object": $tool }')
-        
+
+        field_names=$(echo "$modified_data" | jq -r 'keys[]')
+        for field in $field_names; do
+            fields["$field"]=1
+        done
+
         # Store the modified data in an associative array
         modified_data_array["$key"]=$modified_data
     else
         error_message=$(echo "$data" | jq -r '.errors[0]')
         if [[ "$error_message" == *"permission denied"* ]]; then
             echo "Skipping '$key' due to permission denied error."
+        else
+            echo "Error retrieving data for '$key': $error_message"
         fi
     fi
 done
 
 # Initialize a Markdown table header
 table_header=""
-
-# Loop through the keys and build the header based on available fields
-for key in "${keys[@]}"; do
-    modified_data="${modified_data_array[$key]}"
-    if [ -n "$modified_data" ]; then
-        field_names=$(echo "$modified_data" | jq -r 'keys[]')
-        for field in $field_names; do
-            table_header+="| $field "
-        done
-        break  # Only need the header from the first key
-    fi
+for field in "${!fields[@]}"; do
+    table_header+="| $field "
 done
 table_header+="|"
 
 # Initialize a variable to store Markdown table rows
 table_rows=""
 
+# Enable nocasematch for case-insensitive comparisons
 shopt -s nocasematch
+
 # Loop through the keys again and retrieve JSON data from the array
 for key in "${keys[@]}"; do
     modified_data="${modified_data_array[$key]}"
-    
-    # Check if data exists for this key
-    if [ -n "$modified_data" ]; then
-        table_row=""
-        for field in $table_header; do
-            if [ "$field" != "|" ]; then
-                value=$(echo "$modified_data" | jq -r --arg field "$field" '.[$field] // "N/A"')
-                
-                # Replace values while ignoring case
-                if [[ "$value" == "n/a" ]]; then
-                    value="🚫"
-                elif [[ "$value" == "false" ]]; then
-                    value="❌"
-                elif [[ "$value" == "true" ]]; then
-                    value="✅"
-                fi
-                
-                table_row+="| $value "
-            fi
-        done
-        table_row+="|"
-        table_rows+="\n$table_row"
-    fi
+
+    table_row=""
+    for field in "${!fields[@]}"; do
+        value=$(echo "$modified_data" | jq -r --arg field "$field" '.[$field] // "N/A"')
+
+        # Replace values while ignoring case
+        if [[ "$value" == "n/a" ]]; then
+            value="🚫"
+        elif [[ "$value" == "false" ]]; then
+            value="❌"
+        elif [[ "$value" == "true" ]]; then
+            value="✅"
+        fi
+
+        table_row+="| $value "
+    done
+    table_row+="|"
+    table_rows+="\n$table_row"
 done
+
+# Disable nocasematch
+shopt -u nocasematch
 
 # Add the header separator
 header_separator=$(echo "$table_header" | sed 's/[^|]/-/g')
