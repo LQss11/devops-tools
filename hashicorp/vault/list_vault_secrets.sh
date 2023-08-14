9c63e84cbcc9e6051eb7b1b3985170638c0dd827
Salem Affes <affessalem@hotmail.fr>
2023-08-14 09:17:26 +0100

    list all secrets script

--- hashicorp/vault/list_vault_secrets.sh
+++ hashicorp/vault/list_vault_secrets.sh
@@ -9,13 +9,39 @@ data_array=()
 
 # Loop through the keys and retrieve JSON data for each key
 for key in $keys; do
-    data=$(vault kv get -format=json "secret/$secret_path/$key" | jq -r '.data.data')
-    # Add "tool" field to the JSON data with the corresponding key
-    modified_data=$(echo "$data" | jq --arg tool "$key" '. + { "tool": $tool }')
-    data_array+=("$modified_data")
+    data=$(vault kv get -format=json "secret/$secret_path/$key" 2>/dev/null)
+    
+    # Check if vault kv get was successful
+    if [ $? -eq 0 ]; then
+        # Extract JSON data from the result
+        data=$(echo "$data" | jq -r '.data.data')
+        
+        # Add "tool" field to the JSON data with the corresponding key
+        modified_data=$(echo "$data" | jq --arg tool "$key" '. + { "tool": $tool }')
+        data_array+=("$modified_data")
+    fi
 done
 
 # Join the modified JSON data objects into a single JSON array
 combined_data="[ $(IFS=, ; echo "${data_array[*]}") ]"
 echo $combined_data | jq -r .
-# echo "$combined_data"  | jq -r '[ .[] | keys[] ] | unique | "|" + join(" | ") + " |"'
+
+
+#### POLICY example
+## dev env secrets under paths like this secret/<level1>/<level2>/test
+# path "secret/data/+/+/dev"
+# {
+#   capabilities = ["read"]
+# }
+## Test env secrets under paths like this secret/<level1>/<level2>/test
+# path "secret/data/+/+/test"
+# {
+#   capabilities = ["read"]
+# }
+
+## Allow liting secrets under paths like this secret/<level1>/<level2>/*
+# path "secret/metadata/+/*"
+# {
+#   capabilities = ["list"]
+# }
+
