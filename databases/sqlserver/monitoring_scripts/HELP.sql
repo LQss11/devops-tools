1b619a62a4a46d3dde4df8ea9198e9e13c03443b
Salem Affes <affessalem@hotmail.fr>
2023-10-05 16:06:28 +0100

    Add a sys view monitoring queries help scripts

---
+++ databases/sqlserver/monitoring_scripts/HELP.sql
@@ -0,0 +1,86 @@
+SELECT TOP (1000) *
+  FROM sys.configurations
+
+SELECT TOP (1000) *
+  FROM sys.dm_exec_connections
+
+SELECT TOP (1000) *
+  FROM sys.dm_exec_function_stats
+
+SELECT TOP (1000) *
+  FROM sys.dm_exec_procedure_stats
+
+SELECT TOP (1000) *
+  FROM sys.dm_exec_sessions
+
+SELECT TOP (1000) *
+  FROM sys.dm_os_host_info
+
+SELECT TOP (1000) *
+  FROM sys.dm_os_sys_info
+
+SELECT TOP (1000) *
+  FROM sys.dm_os_memory_nodes
+
+SELECT TOP (1000) *
+  FROM sys.dm_os_process_memory
+
+SELECT TOP (1000) *
+  FROM sys.dm_os_sys_memory
+
+SELECT TOP (1000) *
+  FROM sys.dm_os_performance_counters
+
+
+
+SELECT TOP (1000) *
+  FROM sys.dm_pal_cpu_stats
+
+SELECT TOP (1000) *
+  FROM sys.dm_pal_disk_stats
+
+SELECT TOP (1000) *
+  FROM sys.dm_pal_net_stats
+
+SELECT TOP (1000) *
+  FROM sys.dm_pal_processes
+
+SELECT TOP (1000) *
+  FROM sys.dm_server_services
+
+SELECT TOP (1000) *
+  FROM sys.dm_tran_active_transactions
+-- dm_tran_aborted_transactions
+
+SELECT TOP (1000) *
+  FROM sys.master_files
+
+
+
+SELECT TOP (1000) *
+  FROM sys.messages
+
+SELECT TOP (1000) *
+  FROM sys.schemas
+
+SELECT TOP (1000) *
+  FROM sys.servers
+
+SELECT TOP (1000) *
+  FROM sys.sysperfinfo
+
+SELECT TOP (1000) *
+  FROM sys.sysprocesses
+
+
+
+SELECT TOP (1000) *
+  FROM sys.trace_events
+
+SELECT TOP (1000) *
+  FROM sys.traces
+
+
+
+
+
