8536ae568e359623c2032f980c38ddaede51562c
Salem Affes <affessalem@hotmail.fr>
2023-10-06 09:28:04 +0100

    Advanced details about deadlock

---
+++ databases/sqlserver/monitoring_scripts/deadlock/deadlocks_current_extended_details.sql
@@ -0,0 +1,18 @@
+SELECT
+    TL.request_session_id AS SessionID,
+    TL.resource_type AS ResourceType,
+    DB_NAME(TL.resource_database_id) AS DatabaseName,
+    TL.request_mode AS LockMode,
+    TL.request_status AS LockStatus,
+    -- Enable this if you want more extended and detailed information about the query causing deadlocks (XML)
+    -- EQP.query_plan AS QueryPlan,
+    ER.command AS Command,
+    ES.host_name AS HostName,
+    ES.program_name AS ProgramName,
+    EQT.text AS QueryText
+FROM sys.dm_tran_locks TL
+INNER JOIN sys.dm_exec_requests ER ON TL.request_session_id = ER.session_id
+INNER JOIN sys.dm_exec_sessions ES ON TL.request_session_id = ES.session_id
+CROSS APPLY sys.dm_exec_query_plan(ER.plan_handle) AS EQP
+CROSS APPLY sys.dm_exec_sql_text(ER.sql_handle) AS EQT
+WHERE TL.request_status = 'WAIT';
