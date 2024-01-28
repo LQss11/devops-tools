709253dbc9e1af59f799498fe097dcfbda290c53
LQss11 <affessalem@hotmail.fr>
2024-01-29 00:05:30 +0100

    Add file management commands

--- linux/learn/FILEMANAGE.sh
+++ linux/learn/FILEMANAGE.sh
@@ -5,6 +5,7 @@ ls -lh
 # Zipping
 # Compressfiles
 tar -czvf tar.tar a aa
+tar -C /opt/ -xvf myfile.tar.gz
 # Get files in tar
 tar -tf tar.tar
 # Extract
@@ -20,4 +21,22 @@ bzcat | zcat | xzcat
 # Locate command depend on a db containing all file paths 
 # It might sometimes not work so run updatedb to update it
 locate file.txt 
+
+# Permissions
+chgrp wheel /my/file/example
+chown myuser /my/file/example
+chown myuser:wheel /my/file/example
+
+# SUID SGID (s=can execute S=cannot execute s instead of x) STICKYBIT
+chmod u+s,g+s,o+t /my/file/example
+# SUID file will always be run as default user even if different user
+chmod 4664 /my/file/example
+# SGID file will always run as default group even if ran by different user group
+chmod 2664 /my/file/example
+# SUID + SGID bit
+chmod 6664 /my/file/example
+
+# Sticky bit (allow only owner to delete)
+chmod 1777 /my/file/example
+chmod +t /my/file/example
 
