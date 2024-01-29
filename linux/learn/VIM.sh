23119a5b196f42559163bf3feaef3c3ca4e3c622
LQss11 <affessalem@hotmail.fr>
2024-01-29 03:21:35 +0100

    Vim control commandfs

---
+++ linux/learn/VIM.sh
@@ -0,0 +1,21 @@
+# Search (\c is for case insensitive)
+/something\c
+# Show line numbers
+:set nu
+# Go to specific line
+:15
+# Copy paste
+yy -> p
+# Cut/Delete line
+dd
+# Delete 1000
+1000dd
+# Open new tab (use those to switch gt gT or 4gt for forth tab)
+:tabnew
+# Split horizontal and vertical
+# use ctrl + w then hjkl to move between 
+:sp filename
+:vsp filename
+
+# Change file
+:e! filepath
