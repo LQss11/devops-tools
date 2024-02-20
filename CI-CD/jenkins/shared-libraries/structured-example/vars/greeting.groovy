62d0afc676525181f75d7d467690bd9f72fe6e2a
Salem Affes <affessalem@hotmail.fr>
2024-02-20 09:24:15 +0100

    Add greeting example

--- CI-CD/jenkins/shared-libraries/structured-example/vars/greeting.groovy
+++ CI-CD/jenkins/shared-libraries/structured-example/vars/greeting.groovy
@@ -2,3 +2,10 @@ def call(String name) {
     def message = org.example.Utils.sayHello(name)
     echo message
 }
+
+// another way to import
+// import static com.example.Utils.*
+// def call(String name) {
+//     def message = sayHello(name)
+//     echo message
+// }
