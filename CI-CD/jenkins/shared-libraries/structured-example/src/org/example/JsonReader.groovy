1b7cf705b42bee738ef6b6cc2fef7d363ad8256e
Salem Affes <affessalem@hotmail.fr>
2024-02-27 14:59:01 +0100

    json reader

--- CI-CD/jenkins/shared-libraries/structured-example/src/org/example/JsonReader.groovy
+++ CI-CD/jenkins/shared-libraries/structured-example/src/org/example/JsonReader.groovy
@@ -1,12 +1,16 @@
-package org.exampke
-
 import groovy.json.JsonSlurper
 
-class Bar {
-
-    static String loadName(String key) {
-        def json = new JsonSlurper().parse(Bar.getResourceAsStream("data.json"))
-        return json.names[key]
+class JsonReader {
+    static void main(String[] args) {
+        def json = loadJsonFromFile("resources/data.json")
+        // Access the JSON data as needed
+        println "JSON content:"
+        println json
     }
 
+    static def loadJsonFromFile(String filePath) {
+        def jsonString = new File(filePath).text
+        new JsonSlurper().parseText(jsonString)
+        
+    }
 }
