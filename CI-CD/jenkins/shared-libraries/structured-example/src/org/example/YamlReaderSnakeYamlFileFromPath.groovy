fc7ea5225c2b627efb26bda7ec9580ef690e173d
Salem Affes <affessalem@hotmail.fr>
2024-03-07 14:37:59 +0100

    example yaml reader

---
+++ CI-CD/jenkins/shared-libraries/structured-example/src/org/example/YamlReaderSnakeYamlFileFromPath.groovy
@@ -0,0 +1,42 @@
+package org.example
+@Grab('org.yaml:snakeyaml:1.29')
+import org.yaml.snakeyaml.Yaml
+
+class YamlReader {
+    static void main(String[] args) {
+        def yaml = loadYamlFromFile("resources/data.yaml")
+
+        def appName = System.getenv("APP_NAME")
+
+        if (!appName) {
+            throw new IllegalArgumentException("❌ APP_NAME environment variable is required.")
+        }
+
+        if (yaml."$appName") {
+            def app = yaml."$appName"
+            if (app && app.name) {
+                println "========================================"
+                println "  🚀 Variables for ${appName.capitalize()}:"
+                println "========================================"
+                app.each { key, value ->
+                    println "  ⭐️ ${key.capitalize()}: ${value}"
+                }
+                println "========================================"
+                println "  🌐 Environment Variables:"
+                println "========================================"
+                System.getenv().each { key, value ->
+                    println "  🌟 $key: $value"
+                }
+            } else {
+                throw new IllegalArgumentException("❌ Required attributes 'app' and 'name' are missing for $appName.")
+            }
+        } else {
+            throw new IllegalArgumentException("❌ Invalid value for APP_NAME.")
+        }
+    }
+
+    static def loadYamlFromFile(String filePath) {
+        def yamlString = new File(filePath).text
+        new Yaml().load(yamlString)
+    }
+}
