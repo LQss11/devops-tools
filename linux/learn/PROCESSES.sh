01bbad82aefe7e2f2cfa2e78fa019d8555751071
Salem Affes <affessalem@hotmail.fr>
2024-03-15 11:33:16 +0100

    Add more process management info

--- linux/learn/PROCESSES.sh
+++ linux/learn/PROCESSES.sh
@@ -45,3 +45,46 @@ systemctl unmask atd.server
 # Journalctl
 journalctl -b 
 journalctl -u docker.service
+
+
+# Manage Processes
+ps auxf
+ps -auxf
+# Get process id 
+pgrep -a sshd
+
+# Prioritize processess with nice value (9 is value of ncie 100 is PID)
+sudo nice -n 15 /bin/bash
+# Update nice value of sshd process
+sudo renice 9 $(ps aux | grep -i sshd | awk '{print $2}' | head -1)
+sudo renice 9 $(pgrep sshd)
+
+# get processess nice values
+ps lax
+
+# list files that are opened by process
+sudo lsof $(ps aux | grep -i sshd | awk '{print $2}' | head -1)
+
+# Get information about process logs
+sudo journalctl --unit=sshd.service -n 20 --no-pager
+
+# Send signals (kill -l to list all signals)
+sudo systemctl status sshd.service
+sudo kill -SIGHUP $(pgrep sshd)
+sudo kill -1 $(pgrep sshd)
+
+
+# Get system errors 
+sudo journalctl -p err
+
+# manage background processes
+# run in bg
+sleep 300 &
+# get current jobs in bg
+jobs
+# bring back something to fg (ctrl + z to put back to background but stopped)
+fg 1
+fg $(jobs | grep "sleep 300" | grep -o '[0-9]\+' | head -1)
+# resume to bg 
+bg 1
+bg $(jobs | grep "sleep 300" | grep -o '[0-9]\+' | head -1)
