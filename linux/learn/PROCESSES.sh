98e72cbb2d5a006feeceb96ab5c822e7f25fa1a3
Salem Affes <affessalem@hotmail.fr>
2024-03-15 14:29:19 +0100

    update processes examples

--- linux/learn/PROCESSES.sh
+++ linux/learn/PROCESSES.sh
@@ -1,52 +1,3 @@
-# Systemct commands
-# Path: /etc/systemd/system/myapp.service
-# example file
-[Unit]
-Description=Some script for my app
-Documentation=http://someurl.com
-After= postgresql.server
-[Service]
-ExecStart=/bin/bash /usr/bin/myapp.sh
-User=my_service_user
-Restart=on-failure
-RestartSec=10
-
-[Install]
-WantedBy graphical.target
-
-# More sysctl commands
-systemctl set-default multi-user.target
-systemctl list-units --type service --all
-
-systemctl start myapp.service
-systemctl status myapp.service
-# To reload changes and update for sysctl steps
-systemctl daemon-reload
-systemctl start myapp.service
-
-systemctl edit myapp.service --full
-# before accepting changes if we want to revert we can run
-systemctl revert myapp.service 
-
-systemctl start docker
-systemctl stop docker
-systemctl restart docker
-systemctl reload docker
-systemctl status docker
-# Enable or disable from the sysctl processess on reboot
-systemctl enable docker
-systemctl enable --now mercury.service
-systemctl disable docker
-
-# Mask service to track issues sometimes a service starts another service even when it's stopped
-systemctl mask atd.server
-systemctl unmask atd.server
-
-# Journalctl
-journalctl -b 
-journalctl -u docker.service
-
-
 # Manage Processes
 ps auxf
 ps -auxf
@@ -65,17 +16,10 @@ ps lax
 # list files that are opened by process
 sudo lsof $(ps aux | grep -i sshd | awk '{print $2}' | head -1)
 
-# Get information about process logs
-sudo journalctl --unit=sshd.service -n 20 --no-pager
-
 # Send signals (kill -l to list all signals)
 sudo systemctl status sshd.service
 sudo kill -SIGHUP $(pgrep sshd)
 sudo kill -1 $(pgrep sshd)
-
-
-# Get system errors 
-sudo journalctl -p err
 
 # manage background processes
 # run in bg
