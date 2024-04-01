22e41531b90072cb2b3e83ac08d5dfa10db73f77
Salem Affes <affessalem@hotmail.fr>
2024-04-01 11:56:45 +0100

    Add groups commands

---
+++ linux/learn/GROUPS.sh
@@ -0,0 +1,22 @@
+# Wheel or SUDO group can do anything
+# Primary group is the group for user by default when run commands or processes
+
+# Commands
+groupadd developers
+# add user to group
+gpasswd -a john developers
+# delete user from group
+gpasswd -d john developers
+# check user groups
+groups john
+
+# change primary group
+usermod -g developers john
+# change secondary groups
+usermod -G developers john
+
+# rename group
+groupmod -n programmers developers
+
+# delete group (cannot delete group if it's primary for some user)
+groupdel programmers
