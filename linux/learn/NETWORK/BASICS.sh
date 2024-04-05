12de748acd27fbec222109e48c4609a48ab1f5d6
Salem Affes <affessalem@hotmail.fr>
2024-04-05 09:01:43 +0100

    Add basic networking

---
+++ linux/learn/NETWORK/BASICS.sh
@@ -0,0 +1,17 @@
+# IPv4 (4 groups 32 bits)
+# CIDR notation (Classles Inter-Domain Routing)
+# 192.168.1 -> Network Prefix (Like country prefi +39)
+# 101 -> Host or Device on the network address (Like phone number 123 456 789)
+192.168.1.101/24
+
+# IPv6 (8 groups hexadecimal 128 bits)
+# Example
+2001:db8:3333:4444:5555:6666:7777:8888
+# Shortened IPv6 addresse
+2041:0000:140F:0000:0000:0000:875B:131B ->2041:0:140F::875B:131B
+0000:0000:0000 -> ::
+2041:0000:140F -> 2041:0:140F
+2001:0001 -> 2001:s1
+
+# IPv6 CIDR notation
+2001:db8:3333:4444:5555:6666:7777:8888/64
