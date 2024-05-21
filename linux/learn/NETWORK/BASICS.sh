326533cc8a15cd56af031f6ce23bdc62a0d2451e
Salem Affes <affessalem@hotmail.fr>
2024-05-21 17:02:44 +0100

    fi example

--- linux/learn/NETWORK/BASICS.sh
+++ linux/learn/NETWORK/BASICS.sh
@@ -2,7 +2,7 @@
 # CIDR notation (Classles Inter-Domain Routing)
 # 192.168.1 -> Network Prefix (Like country prefi +39)
 # 101 -> Host or Device on the network address (Like phone number 123 456 789)
-192.168.1.101/24
+192.168.12.101/24
 
 # IPv6 (8 groups hexadecimal 128 bits)
 # Example
@@ -11,7 +11,7 @@
 2041:0000:140F:0000:0000:0000:875B:131B ->2041:0:140F::875B:131B
 0000:0000:0000 -> ::
 2041:0000:140F -> 2041:0:140F
-2001:0001 -> 2001:s1
+2001:0001 -> 2001:1
 
 # IPv6 CIDR notation
 2001:db8:3333:4444:5555:6666:7777:8888/64
