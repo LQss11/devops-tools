d051d358b173c0db3d1ce47b9aefaced06ce5828
Affes Salem <49565791+LQss11@users.noreply.github.com>
2024-07-16 23:47:55 +0100

    Update CONFIG.sh

--- git/CONFIG.sh
+++ git/CONFIG.sh
@@ -1,7 +1,7 @@
 git config --global user.email "myuser"
 git config --global user.name "example@email.com"
 cat .git.config 
-
+ 
 # Normalize and disable line ending conversion.
 git config --global core.eol lf
 git config --global core.autocrlf false
