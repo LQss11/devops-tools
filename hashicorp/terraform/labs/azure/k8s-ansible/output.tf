25a56e1340bfd1d69c1884adb6d5e25e94027e3f
affessalem@hotmail.fr <LQss11>
2024-07-16 23:24:26 +0100

    Add output

--- hashicorp/terraform/labs/azure/k8s-ansible/output.tf
+++ hashicorp/terraform/labs/azure/k8s-ansible/output.tf
@@ -1,9 +1,9 @@
 output "public_ip_address" {
-  value = module.ansible.public_ip_addresses
+  value = module.ansible.public_ip_addresses[0]
 }
 output "ssh_commands" {
   value = [
-    for ip in module.ansible.public_ip_addresses : "ssh -i .ssh/id_rsa lqss@${ip}"
+    for ip in module.ansible.public_ip_addresses : "ssh -i .ssh/id_rsa ${var.username}@${ip}"
   ]
 }
 output "ansible" {
@@ -11,4 +11,7 @@ output "ansible" {
 }
 output "master_nodes" {
   value = module.master.private_ips
+}
+output "worker_nodes" {
+  value = module.worker.private_ips
 }
