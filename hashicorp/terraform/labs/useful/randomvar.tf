e19f7889e1e8720fe710131e80ec1c37f1a90ba9
Salem Affes <affessalem@hotmail.fr>
2024-09-24 19:55:40 +0100

    Update other example of random var

--- hashicorp/terraform/labs/useful/randomvar.tf
+++ hashicorp/terraform/labs/useful/randomvar.tf
@@ -4,3 +4,19 @@ resource "random_id" "main" {
 locals {
   random_var = lower(replace(random_id.main.id, "/[^a-zA-Z0-9]/", ""))
 }
+
+resource "random_string" "suffix" {
+  length  = 8
+  upper   = false
+  special = false
+  numeric = true
+  lower   = true
+}
+
+resource "random_integer" "example" {
+  min = 100
+  max = 999
+  keepers = {
+    first = timestamp()
+  }
+}
